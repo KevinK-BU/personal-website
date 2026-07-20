51f560821d076c3fea2e8a20e88e65b69721184f
Kevin Kupeli <kkupeli@bu.edu>
2026-07-20 11:08:51 -0400

    Updated projects page and hackathon projects page to include new projects and new handling/styling for project links

--- site.js
+++ site.js
@@ -76,3 +76,55 @@ function prevImageConnect() {
 currentConnect = (currentConnect - 1 + imagesConnect.length) % imagesConnect.length;
 lightboxImgConnect.src = imagesConnect[currentConnect].src;
 }
+
+//FashionForward Lightbox
+let currentFashion = 0;
+const imagesFashion = document.querySelectorAll('.image-container img[src*="fashion-forward-screenshots"]');
+const lightboxFashion = document.getElementById('lightbox-fashion');
+const lightboxImgFashion = document.getElementById('lightbox-img-fashion');
+
+function openLightboxFashion(index) {
+currentFashion = index;
+lightboxImgFashion.src = imagesFashion[currentFashion].src;
+lightboxFashion.classList.remove('hidden');
+}
+
+function closeLightboxFashion() {
+lightboxFashion.classList.add('hidden');
+}
+
+function nextImageFashion() {
+currentFashion = (currentFashion + 1) % imagesFashion.length;
+lightboxImgFashion.src = imagesFashion[currentFashion].src;
+}
+
+function prevImageFashion() {
+currentFashion = (currentFashion - 1 + imagesFashion.length) % imagesFashion.length;
+lightboxImgFashion.src = imagesFashion[currentFashion].src;
+}
+
+//Clock In 2 Lightbox
+let currentClock2 = 0;
+const imagesClock2 = document.querySelectorAll('.image-container img[src*="clock-in-2-screenshots"]');
+const lightboxClock2 = document.getElementById('lightbox-clock-2');
+const lightboxImgClock2 = document.getElementById('lightbox-img-clock-2');
+
+function openLightboxClock2(index) {
+currentClock2 = index;
+lightboxImgClock2.src = imagesClock2[currentClock2].src;
+lightboxClock2.classList.remove('hidden');
+}
+
+function closeLightboxClock2() {
+lightboxClock2.classList.add('hidden');
+}
+
+function nextImageClock2() {
+currentClock2 = (currentClock2 + 1) % imagesClock2.length;
+lightboxImgClock2.src = imagesClock2[currentClock2].src;
+}
+
+function prevImageClock2() {
+currentClock2 = (currentClock2 - 1 + imagesClock2.length) % imagesClock2.length;
+lightboxImgClock2.src = imagesClock2[currentClock2].src;
+}
